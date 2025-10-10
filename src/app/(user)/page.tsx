372ca3420bcde6e7a13c458126d406bb3f1f47f5
Ahmed Omar <ahmed.omar.platform@gmail.com>
2025-10-10 12:58:11 +0300

    added hero section

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div>
-      <span className="">Hello Home</span>
-    </div>
+    <main className="flex flex-col justify-center items-center py-6">
+      <h1 className="text-5xl text-slate-200">Hello GUCian 👋</h1>
+      <p className="py-8 px-8 text-lg text-center text-slate-50">
+        GUC Tutor is tailor made for you, first year students, to make the start
+        of your journey of becoming an Engineer as smooth as possible.
+      </p>
+      <Button variant="action" className="cursor-pointer">
+        Let Get Started
+      </Button>
+    </main>
   );
 };
 
